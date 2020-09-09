2a869e6749a621b293bc450f13c6f33627ebb16e
Adin Lukas DelaCruz <lukeisun57@gmail.com>
2020-09-09 13:04:21 -0400

    First decomposition function in encrypter is strToInt, which takes a string and turns it into an array of numbers

--- delacruz_p1/src/Encrypter.java
+++ delacruz_p1/src/Encrypter.java
@@ -2,15 +2,18 @@ public class Encrypter {
     public String encrypt(String toBeEncrypted){
         String retString = "";
         int radix = 10;
-        int[] asciiVal = new int[4];
-        for(int i=0; i<toBeEncrypted.length(); i++){
-            asciiVal[i] = (Character.getNumericValue(toBeEncrypted.charAt(i)) + 7)%10;
-        }
-        retString += Character.forDigit(asciiVal[2],radix);
-        retString += Character.forDigit(asciiVal[3], radix);
-        retString += Character.forDigit(asciiVal[0],radix);
-        retString += Character.forDigit(asciiVal[1],radix);
+        int[] strToIntArr = strToInt(toBeEncrypted);
+        retString += Character.forDigit(strToIntArr[2],radix);
+        retString += Character.forDigit(strToIntArr[3], radix);
+        retString += Character.forDigit(strToIntArr[0],radix);
+        retString += Character.forDigit(strToIntArr[1],radix);
         return retString;
-
+    }
+    private int[] strToInt(String s){
+        int[] val = new int[4];
+        for(int i=0; i<s.length(); i++){
+            val[i] = (Character.getNumericValue(s.charAt(i)) + 7)%10;
+        }
+        return val;
     }
 }
