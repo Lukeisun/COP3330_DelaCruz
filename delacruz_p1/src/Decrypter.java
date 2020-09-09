bd5d52ea60dca480b5047f71e74fdcf8403c1b73
Adin Lukas DelaCruz <lukeisun57@gmail.com>
2020-09-09 14:09:29 -0400

    Program pretty much done, maybe add some comments? Added second func of decomp in decrypt as well. Just takes the integer of ints and makes them into a string to be ret'd

--- delacruz_p1/src/Decrypter.java
+++ delacruz_p1/src/Decrypter.java
@@ -1,23 +1,27 @@
 public class Decrypter {
     public String decrypt(String toBeDecrypted){
         int[] strToIntArr = strToInt(toBeDecrypted);
-        String retString = "";
-
+        String retString = makingRetVal(strToIntArr);
         return retString;
     }
-    public int[] strToInt(String s){
+    private int[] strToInt(String s){
         int[] retVal = new int[s.length()];
         for(int i=0; i<s.length(); i++){
             char c = s.charAt(i);
             int valOfIdx = Character.getNumericValue(c);
-
             if(valOfIdx <= 3){
                 retVal[i] = valOfIdx + 3;
             } else {
                 retVal[i] = valOfIdx - 7;
             }
-            System.out.println(retVal[i]);
         }
         return retVal;
+    }
+    private String makingRetVal(int[] val){
+        String ret = "";
+        int radix = 10;
+        int[] orderToChange = {2,3,0,1};
+        for(int i: orderToChange) ret += Character.forDigit(val[i], radix);
+        return ret;
     }
 }
