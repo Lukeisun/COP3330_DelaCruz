0281b79c62932a01f4ec3695b4d671251f765b8d
Adin Lukas DelaCruz <lukeisun57@gmail.com>
2020-09-09 13:31:06 -0400

    Added last decomposition function for encryptor, all is good with it

--- delacruz_p1/src/Encrypter.java
+++ delacruz_p1/src/Encrypter.java
@@ -1,19 +1,25 @@
-public class Encrypter {
+public class Encrypter{
     public String encrypt(String toBeEncrypted){
-        String retString = "";
-        int radix = 10;
-        int[] strToIntArr = strToInt(toBeEncrypted);
-        retString += Character.forDigit(strToIntArr[2],radix);
-        retString += Character.forDigit(strToIntArr[3], radix);
-        retString += Character.forDigit(strToIntArr[0],radix);
-        retString += Character.forDigit(strToIntArr[1],radix);
-        return retString;
+        int[] strToIntArr;
+        strToIntArr = strToInt(toBeEncrypted);
+        String encryptedString;
+        encryptedString = makingRetVal(strToIntArr);
+        return encryptedString;
     }
+
     private int[] strToInt(String s){
         int[] val = new int[4];
         for(int i=0; i<s.length(); i++){
             val[i] = (Character.getNumericValue(s.charAt(i)) + 7)%10;
         }
         return val;
+    }
+
+    private String makingRetVal(int[] val){
+        String ret = "";
+        int radix = 10;
+        int[] orderToChange = {2,3,0,1};
+        for (int j : orderToChange) ret += Character.forDigit(val[j], radix);
+        return ret;
     }
 }
